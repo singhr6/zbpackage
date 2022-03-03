31296aef209a5d02f60ff5ced1a568feb1042da0
singhr6 <60188465+singhr6@users.noreply.github.com>
2022-03-02 20:38:58 -0500

    -removed import json -Added comments for functions

--- zbpackage/argos_utils.py
+++ zbpackage/argos_utils.py
@@ -2,8 +2,13 @@
 This module contains several function related to argos system.
 '''
 from influxdb import InfluxDBClient
-import json
 
+
+'''
+This function is used to update argos. It follow a payload format - measurement,tag,tagValue,value
+curl -i -XPOST http://tibco-app-vp05:8086/write?db=telegraf --data-binary "job_status,job=mft_cleanup value=1"
+You can manipulate value parameters to match with grafana value and setting.
+'''
 def update_influxdb(measurement,tag,tag_value,value):
     try:
         host='tibco-app-vp05'
@@ -28,5 +33,7 @@ def update_influxdb(measurement,tag,tag_value,value):
     finally:
         client.close()
 
+# local testing of function
 if __name__=="__main__":
-    print(update_influxdb('node_mem','node','AN_LB01_VD03',23.0))
+    print(update_influxdb('node_mem','node','AN_LB01_VD03',90.0))
+    print(update_influxdb('job_status','job','mft_cleanup',0))
